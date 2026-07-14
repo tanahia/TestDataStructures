e81024e823d6c5ca9fa16349a839f97e17728907
tanahia <sofi.bookfan@gmail.com>
2026-07-14 15:28:36 +0100

    Used Binary Search method for inventory
    Signed-off-by: tanahia <sofi.bookfan@gmail.com>

--- TestDataStructures/Assets/Scripts/SearchMethods.cs
+++ TestDataStructures/Assets/Scripts/SearchMethods.cs
@@ -19,4 +19,37 @@ public class SearchMethods : MonoBehaviour
                 Debug.Log("Item not found");
         }
     }
+    public int BinarySearch()
+    {
+        string key = input.HandOverInput();
+        int left = 0;
+        int right = inventoryItems.Length - 1;
+
+        // Array is needed to be soted for binary search
+        System.Array.Sort(inventoryItems);
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            int comparison = string.Compare(inventoryItems[mid], key, System.StringComparison.Ordinal);
+
+            if (comparison == 0)
+                return mid;
+            else if (comparison > 0)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        return -1;
+    }
+    public void DisplayBinarySearchResult()
+    {
+        int result = BinarySearch();
+        if (result != -1)
+            Debug.Log("Item found: " + inventoryItems[result]);
+        else
+            Debug.Log("Item not found");
+    }
 }
